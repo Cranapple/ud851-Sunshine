8b8bef055c1648fdbbe36cabdb37bf221b45d2b7
Lucas Tecot <lucastecot@gmail.com>
2017-01-09 20:49:53 -0800

    RecyclerView

---
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/utilities/ForecastAdapter.java
@@ -0,0 +1,64 @@
+package com.example.android.sunshine.utilities;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.android.sunshine.R;
+
+/**
+ * Created by lucastecot on 1/9/17.
+ */
+
+public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder> {
+
+    private String[] mWeatherData;
+
+    public ForecastAdapter() {
+
+    }
+
+    @Override
+    public ForecastAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int layoutID = R.layout.forecast_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(layoutID, parent, false);
+        ForecastAdapterViewHolder viewHolder = new ForecastAdapterViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ForecastAdapterViewHolder holder, int position) {
+
+        holder.mWeatherTextView.setText(mWeatherData[position]);
+    }
+
+    public void setWeatherData(String[] weatherData) {
+        mWeatherData = weatherData;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mWeatherData == null)
+            return 0;
+        else
+            return mWeatherData.length;
+    }
+
+    public class ForecastAdapterViewHolder extends  RecyclerView.ViewHolder{
+
+        public final TextView mWeatherTextView;
+
+        public ForecastAdapterViewHolder(View view) {
+            super(view);
+            mWeatherTextView = (TextView) view.findViewById(R.id.tv_weather_data);
+        }
+
+    }
+
+}
